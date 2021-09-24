a7de10cb467d0390935bc574cbbdcf96acd463c5
ramitdour <ramit.dour@gmail.com>
2021-09-24 15:14:31 +0530

    cin cout

---
+++ 1.0 Hello World/inputOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ // Single line comment #1
+    int x = 0;
+    cout << "value of x = " << endl; // Single line comment #2
+
+    cout << "enter value of 'x' " << endl;
+    cin >> x;
+
+    cout << "new value of x = " << endl; // Single line comment #2
+}
